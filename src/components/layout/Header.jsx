a05cb5afccd35d8dcb30b978ff924c19b3ecab8a
andrea-mosca <andrea.moscateli@gmail.com>
2025-07-23 16:00:45 +0200

    end

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <h1 className="py-3 ms-5">Learn Web development</h1>
+      <h1 className="text-center py-3 ms-5">Learn Web development</h1>
     </header>
   );
 }
